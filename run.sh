c0d4d471f90030b2feafab2842ea02c1fa9f5995
Jannot <jannod@frenjannodm1.corp.emc.com>
2015-06-28 22:52:22 +0200

    Adding ECS Management API ports

--- run.sh
+++ run.sh
@@ -20,9 +20,9 @@ for i in {1..2}; do
 done
 chmod -R 777 /disks
 
-docker run -d -e SS_GENCONFIG=1 --name=ecs1 --hostname=ecs1.localdomain -p 443:443 -p 9020:9020 -p 9021:9021 -p 9022:9022 -p 9023:9023 -p 9024:9024 -p 9025:9025 -v /network.json.10.0.0.1:/host/data/network.json -v /seeds1:/host/files/seeds -v /partitions.json:/data/partitions.json -v /disks/1:/disks -v /var/log/ecs/1:/opt/storageos/logs djannot/es:v2.0
+docker run -d -e SS_GENCONFIG=1 --name=ecs1 --hostname=ecs1.localdomain -p 443:443 -p 4443:4443 -p 8443:8443 -p 9020:9020 -p 9021:9021 -p 9022:9022 -p 9023:9023 -p 9024:9024 -p 9025:9025 -v /network.json.10.0.0.1:/host/data/network.json -v /seeds1:/host/files/seeds -v /partitions.json:/data/partitions.json -v /disks/1:/disks -v /var/log/ecs/1:/opt/storageos/logs djannot/es:v2.0
 sleep 5
 docker exec -i -t ecs1 chmod -R 777 /host
-docker run -d -e SS_GENCONFIG=1 --name=ecs2 --hostname=ecs2.localdomain -p 2443:443 -p 29020:9020 -p 29021:9021 -p 29022:9022 -p 29023:9023 -p 29024:9024 -p 29025:9025 -v /network.json.10.0.0.2:/host/data/network.json -v /seeds2:/host/files/seeds -v /partitions.json:/data/partitions.json -v /disks/2:/disks -v /var/log/ecs/2:/opt/storageos/logs djannot/es:v2.0
+docker run -d -e SS_GENCONFIG=1 --name=ecs2 --hostname=ecs2.localdomain -p 2443:443 -p 24443:4443 -p 28443:8443 -p 29020:9020 -p 29021:9021 -p 29022:9022 -p 29023:9023 -p 29024:9024 -p 29025:9025 -v /network.json.10.0.0.2:/host/data/network.json -v /seeds2:/host/files/seeds -v /partitions.json:/data/partitions.json -v /disks/2:/disks -v /var/log/ecs/2:/opt/storageos/logs djannot/es:v2.0
 sleep 5
 docker exec -i -t ecs2 chmod -R 777 /host
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ for i in {2..63}; do if [ -e /dev/loop$i ]; then continue; fi; \
 mknod /dev/loop$i b 7 $i; chown --reference=/dev/loop0 /dev/loop$i; \
 chmod --reference=/dev/loop0 /dev/loop$i; done
 sysctl -w kernel.pid_max=655360
-docker run --privileged -t -i -p 443:443 -p 2443:2443 -p 9020:9020 -p 9021:9021 -p 9022:9022 -p 9023:9023 -p 9024:9024 -p 9025:9025 -p 29020:29020 -p 29021:29021 -p 29022:29022 -p 29023:29023 -p 29024:29024 -p 29025:29025 $1
+docker run --name docker --privileged -d -p 443:443 -p 2443:2443 -p 4443:4443 -p 24443:24443 -p 8443:8443 -p 28443:28443 -p 9020:9020 -p 9021:9021 -p 9022:9022 -p 9023:9023 -p 9024:9024 -p 9025:9025 -p 29020:29020 -p 29021:29021 -p 29022:29022 -p 29023:29023 -p 29024:29024 -p 29025:29025 $1
